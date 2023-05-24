5a07b5fdac1273daeb5ccb90217fbbd9344ae0e9
ArlessonMoura <arlessonmss@gmail.com>
2023-05-24 20:24:07 -0300

    🎨 Full Build frontend  (React Vite)

---
+++ frontend-welcome/.eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb-typescript/base',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json',
+  },
+  plugins: ['react-refresh', 'import'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'import/prefer-default-export': 'off',
+    'react/prop-types': 'off',
+    'no-plusplus': 'off',
+  },
+};
