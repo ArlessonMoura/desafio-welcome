053fa11ee3ea9ca58c6fd415d02bd5e6dff98359
ArlessonMoura <arlessonmss@gmail.com>
2023-05-24 22:40:10 -0300

    🚨 Fix ESlint Import Rules

--- frontend-welcome/.eslintrc.cjs
+++ frontend-welcome/.eslintrc.cjs
@@ -21,5 +21,6 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'react/prop-types': 'off',
     'no-plusplus': 'off',
+    'import/extensions': ['error', 'never', { css: 'always', scss: 'always' }],
   },
 };
